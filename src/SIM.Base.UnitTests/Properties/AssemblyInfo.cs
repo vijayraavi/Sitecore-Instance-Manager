119168ec139008d9af2b0808e98f211dd2b547d2
AlenPelin <AlenPelin@gmail.com>
2017-05-30 12:41:07 +1000

    Add unit tests

---
+++ src/SIM.Base.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SIM.Base.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SIM.Base.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("883085b5-6e2b-4733-98f9-00c3dd8e4ce1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/SIM.Base.UnitTests/SqlConnectionString_Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace SIM
+{
+  using System;
+  using SIM.Base.Services;
+  using Xunit;
+
+  public class SqlConnectionString_Tests
+  {
+    [Fact]
+    public void Create_Valid()
+    {
+      var connect = new SqlConnectionString("Data Source=.\\SQL2016; User ID=sa; Password=12345");
+
+      Assert.Equal("Data Source=.\\SQL2016; User ID=sa; Password=12345", connect.Value);
+      Assert.Equal(".\\SQL2016", connect.Builder.DataSource);
+      Assert.Equal("sa", connect.Builder.UserID);
+      Assert.Equal("12345", connect.Builder.Password);
+    }
+
+    [Fact]
+    public void Create_Invalid()
+    {
+      Assert.Throws<ArgumentException>(() => new SqlConnectionString("Data Sour=.\\SQL2016"));
+    }
+  }
+}
