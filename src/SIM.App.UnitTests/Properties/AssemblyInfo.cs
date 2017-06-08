761adda09fa5251621bbb650f99710727fab81f3
AlenPelin <AlenPelin@gmail.com>
2017-06-08 10:24:24 +1000

    Fix assembly info patching by appveryor (closes #175)

--- src/SIM.App.UnitTests/Properties/AssemblyInfo.cs
+++ src/SIM.App.UnitTests/Properties/AssemblyInfo.cs
@@ -6,6 +6,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("SIM")]
 [assembly: ComVisible(false)]
 
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
+
 #endregion
 
 [assembly: AssemblyTitle("SIM.App.UnitTests")]
--- src/SIM.Base.UnitTests/Properties/AssemblyInfo.cs
+++ src/SIM.Base.UnitTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,15 @@
+#region Global
+
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SIM.Base.UnitTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SIM.Base.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyProduct("SIM")]
 [assembly: ComVisible(false)]
 
-[assembly: Guid("883085b5-6e2b-4733-98f9-00c3dd8e4ce1")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
 
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+#endregion
+
+[assembly: AssemblyTitle("SIM.Base.UnitTests")]
